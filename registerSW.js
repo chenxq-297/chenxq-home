e45f97dffd43ef85c4dc40e7d3fdce03ccd288db
chenxq-297 <83133409+chenxq-297@users.noreply.github.com>
2024-02-25 13:48:19 +0000

    Deploying to dev from @ chenxq-297/chenxq-home@b06e507f59967843e9d685b7983fb1c792c9b345 🚀

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/chenxq-home/sw.js', { scope: '/chenxq-home/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
